d936fd4dbe4ee3475cbf3126dd58bbf97a4260bc
Marcus.Ertl <Marcus.Ertl@daeca3e4-217b-11df-99f8-b513f4f1e6c6>
2010-02-24 22:19:38 +0000

    Changes by going live!

--- dca/tl_settings.php
+++ dca/tl_settings.php
@@ -2,26 +2,26 @@
 /**
  * TYPOlight Cron Scheduler
  *
- * Cron is a scheduler module for the TYPOlight CMS. It allows to automaticly 
- * execute php on a time schedule similar to the unix cron/crontab scheme.  
+ * Cron is a scheduler module for the TYPOlight CMS. It allows to automaticly
+ * execute php on a time schedule similar to the unix cron/crontab scheme.
  * TYPOlight is a web content management system that specializes in accessibility
  * and generates W3C-compliant HTML code.
  *
- * If you need to contact the author of this module, please use the forum at 
- * http://www.typolight.org/forum. Additional documentation can be found at the 
+ * If you need to contact the author of this module, please use the forum at
+ * http://www.typolight.org/forum. Additional documentation can be found at the
  * 3rd party extensions WIKI http://www.typolight.org/wiki/extensions:extensions
- * For more information about TYPOlight and additional applications please visit 
- * the project website http://www.typolight.org. 
+ * For more information about TYPOlight and additional applications please visit
+ * the project website http://www.typolight.org.
  *
  * NOTE: this file was edited with tabs set to 4.
  *
  * Extends module tl_settings.
  *
  * PHP version 5
- * @copyright  
- * @author     
+ * @copyright
+ * @author
  * @package    CiviCRM
- * @license    
+ * @license
  * @filesource
  */
 
@@ -29,7 +29,7 @@
  * Add to palette
  */
 
-$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] .= ';{civicrm_legend},civicrm_apiurl,civicrm_sitekey,civicrm_apikey,civicrm_membergroup'; 
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] .= ';{civicrm_legend},civicrm_apiurl,civicrm_sitekey,civicrm_apikey,civicrm_membergroup';
 
 /**
  * Add field
@@ -56,11 +56,19 @@ $GLOBALS['TL_DCA']['tl_settings']['fields']['civicrm_apikey'] = array(
 	'eval'		=> array('mandatory'=>true, 'rgxp'=>'extnd', 'nospace'=>true, 'tl_class'=>'w50')
 );
 
+//$GLOBALS['TL_DCA']['tl_settings']['fields']['civicrm_membergroup'] = array(
+//	'label'		=> &$GLOBALS['TL_LANG']['tl_settings']['civicrm_membergroup'],
+//	'inputType'	=> 'text',
+//	'default'	=> '',
+//	'eval'		=> array('mandatory'=>true, 'rgxp'=>'digit', 'tl_class'=>'w50')
+//);
+
 $GLOBALS['TL_DCA']['tl_settings']['fields']['civicrm_membergroup'] = array(
 	'label'		=> &$GLOBALS['TL_LANG']['tl_settings']['civicrm_membergroup'],
-	'inputType'	=> 'text',
+	'inputType'	=> 'select',
+	'options_callback' => array('CiviCRM', 'getAllGroups'),
 	'default'	=> '',
-	'eval'		=> array('mandatory'=>true, 'rgxp'=>'digit', 'tl_class'=>'w50')
+	'eval'		=> array('mandatory'=>true, 'tl_class'=>'w50')
 );
 
 ?>
